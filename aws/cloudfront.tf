70bf2178b14f22f23440794ec8e2ea2c341f6722
Eric Naeseth <eric@naeseth.com>
2017-02-15 18:54:25 -0800

    Use a custom origin for CloudFront instead of S3
    This allows index.html pages outside the root directory to be
    rendered, fixing the 404 for /first-burn/
    
    https://forums.aws.amazon.com/thread.jspa?threadID=85849

--- aws/cloudfront.tf
+++ aws/cloudfront.tf
@@ -18,10 +18,13 @@ resource "aws_cloudfront_distribution" "naeseth" {
 
   origin {
     origin_id   = "naeseth_s3"
-    domain_name = "${aws_s3_bucket.naeseth.bucket_domain_name}"
+    domain_name = "${aws_s3_bucket.naeseth.website_endpoint}"
 
-    s3_origin_config {
-      origin_access_identity = "${aws_cloudfront_origin_access_identity.naeseth.cloudfront_access_identity_path}"
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
     }
   }
 
